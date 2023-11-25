008adfb2ff814e132e4ce495bf10f8a36fd3044b
Said Muhammad Mazaya <saidmuhammad572@gmail.com>
2023-11-25 15:52:55 +0700

    Student Detail

--- University Management System/src/university/management/system/StudentDetails.java
+++ University Management System/src/university/management/system/StudentDetails.java
@@ -8,35 +8,36 @@ import java.awt.event.*;
 
 public class StudentDetails extends JFrame implements ActionListener {
 
-    Choice crollno;
+    Choice lbnim;
     JTable table;
     JButton search, print, update, add, cancel;
-    
+
     StudentDetails() {
-        
+
         getContentPane().setBackground(Color.WHITE);
         setLayout(null);
-        
+
         JLabel heading = new JLabel("Search by Roll Number");
         heading.setBounds(20, 20, 150, 20);
         add(heading);
-        
-        crollno = new Choice();
-        crollno.setBounds(180, 20, 150, 20);
-        add(crollno);
-        
+
+        lbnim = new Choice();
+        lbnim.setBounds(180, 20, 150, 20);
+        lbnim.insert("Semua Data", 0);
+        add(lbnim);
+
         try {
             Conn c = new Conn();
             ResultSet rs = c.s.executeQuery("select * from student");
             while(rs.next()) {
-                crollno.add(rs.getString("rollno"));
+                lbnim.add(rs.getString("nim"));
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
+
         table = new JTable();
-        
+
         try {
             Conn c = new Conn();
             ResultSet rs = c.s.executeQuery("select * from student");
@@ -44,50 +45,70 @@ public class StudentDetails extends JFrame implements ActionListener {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
+
         JScrollPane jsp = new JScrollPane(table);
         jsp.setBounds(0, 100, 900, 600);
         add(jsp);
-        
+
         search = new JButton("Search");
         search.setBounds(20, 70, 80, 20);
         search.addActionListener(this);
         add(search);
-        
+
         print = new JButton("Print");
         print.setBounds(120, 70, 80, 20);
         print.addActionListener(this);
         add(print);
-        
+
         add = new JButton("Add");
         add.setBounds(220, 70, 80, 20);
         add.addActionListener(this);
         add(add);
-        
+
         update = new JButton("Update");
         update.setBounds(320, 70, 80, 20);
         update.addActionListener(this);
         add(update);
-        
+
         cancel = new JButton("Cancel");
         cancel.setBounds(420, 70, 80, 20);
         cancel.addActionListener(this);
         add(cancel);
-        
+
         setSize(900, 700);
         setLocation(300, 100);
         setVisible(true);
     }
-    
+
     public void actionPerformed(ActionEvent ae) {
         if (ae.getSource() == search) {
-            String query = "select * from student where rollno = '"+crollno.getSelectedItem()+"'";
-            try {
-                Conn c = new Conn();
-                ResultSet rs = c.s.executeQuery(query);
-                table.setModel(DbUtils.resultSetToTableModel(rs));
-            } catch (Exception e) {
-                e.printStackTrace();
+            String selectedNim = lbnim.getSelectedItem();
+
+            if ("Semua Data".equals(selectedNim)) {
+                // Show all data without considering nim
+                try {
+                    Conn c = new Conn();
+                    ResultSet rs = c.s.executeQuery("select * from student");
+                    table.setModel(DbUtils.resultSetToTableModel(rs));
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else {
+                // Search by the selected nim
+                String query = "select * from student where nim = '" + selectedNim + "'";
+                try {
+                    Conn c = new Conn();
+                    ResultSet rs = c.s.executeQuery(query);
+                    table.setModel(DbUtils.resultSetToTableModel(rs));
+
+                    // Show Add, Update, Print buttons when a specific nim is selected
+                    add.setVisible(true);
+                    update.setVisible(true);
+                    print.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         } else if (ae.getSource() == print) {
             try {
@@ -105,6 +126,7 @@ public class StudentDetails extends JFrame implements ActionListener {
             setVisible(false);
         }
     }
+
 
     public static void main(String[] args) {
         new StudentDetails();
